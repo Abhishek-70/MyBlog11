45479b8daabcd498a77a85bf78c00d310c4a8c7c
Abhishek Singh <abhi2031999@gmail.com>
2024-01-17 23:55:14 +0530

    128:Created feature to Handle The Specific Exception Named as ResourceNotFoundException Occured while Bad User Input Is Given,

---
+++ src/main/java/com/myblog11/myblog11/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.myblog11.myblog11.exception;
+
+import com.myblog11.myblog11.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)  //used to define a specific exception class will gonna Handled Here
+    public ResponseEntity<ErrorDetails> handleIndResourceNotFoundException(
+            ResourceNotFoundException r,
+            WebRequest webrequest
+    ){
+        ErrorDetails errorDetails=new ErrorDetails(r.getMessage(),new Date(),webrequest.getDescription(true));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
